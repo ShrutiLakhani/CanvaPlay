b6a99c3bb70e631b9d2616cf82795895f2daaa7c
ShrutiLakhani <82932172+ShrutiLakhani@users.noreply.github.com>
2022-06-17 23:35:28 +0530

    Added VideoCard/Recommended Videos Page

--- src/Pages/Explore/Explore.jsx
+++ src/Pages/Explore/Explore.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import "./Explore.css";
+import { Sidebar, VideoCard } from "../../Components/components";
+import { useVideo } from "../../context/context";
 function Explore() {
+  console.log("comes");
+  const { allVideos, fetchVideos } = useVideo();
+  console.log(allVideos);
   return (
     <>
-      <div>
-        <h1>Explore</h1>
+      <div className="videolib-wrapper">
+        <h2>Recommended</h2>
+        <div className="video-pages">
+          <Sidebar />
+          <div className="recommended-videos">
+            {allVideos.map((video) => (
+              <VideoCard key={video._id} video={video} />
+            ))}
+          </div>
+        </div>
       </div>
     </>
   );
